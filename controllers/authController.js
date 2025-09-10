f17d140682e14c62575143e36f750579d8165639
vaibhav <codweird5@gmail.com>
2025-09-10 17:24:42 +0530

    added new controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -69,3 +69,12 @@ exports.getProfile = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find().select("-password"); 
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const { protect, authorize } = require('../middleware/authMiddleware');
 const { getAdminDashboard } = require('../controllers/adminController');
-
+const { getAllUsers } = require('../controllers/authController');
 const router = express.Router();
 
 router.get('/dashboard', protect, authorize('admin'), getAdminDashboard);
-
+router.get('/users', protect , authorize('admin'), getAllUsers);
 module.exports = router;
