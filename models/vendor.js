cef7c2fc6d6ec9961364d0dfd2c6f586e8508a4e
Akash Dhiman <akashdhiman96252@gmail.com>
2025-09-20 16:14:53 +0530

    Added

--- models/vendor.js
+++ models/vendor.js
@@ -1,12 +1,34 @@
-// models/vendor.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const vendorSchema = new mongoose.Schema({
-  code: {type: String, unique: true},
-  name: { type: String, required: true, unique: true },
+  code: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
   address: { type: String },
   state: { type: String },
-  gstNumber: { type: String }
+  gstNumber: { type: String },
 }, { timestamps: true });
 
-module.exports = mongoose.model('Vendor', vendorSchema);
+vendorSchema.pre("save", async function (next) {
+  if (!this.isNew) return next(); 
+
+  try {
+    const prefix = "GC/SC/";
+
+    const lastVendor = await this.constructor.findOne().sort({ createdAt: -1 });
+
+    let nextNumber = 1;
+    if (lastVendor && lastVendor.code) {
+      const match = lastVendor.code.match(/(\d+)$/);
+      if (match) {
+        nextNumber = parseInt(match[1], 10) + 1;
+      }
+    }
+
+    this.code = prefix + String(nextNumber).padStart(4, "0");
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = mongoose.model("Vendor", vendorSchema);
