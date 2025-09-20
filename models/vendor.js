a7991af901996a5b38b50401ff07bf85226385c0
Akash Dhiman <akashdhiman96252@gmail.com>
2025-09-20 16:24:42 +0530

    D;

--- models/vendor.js
+++ models/vendor.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const vendorSchema = new mongoose.Schema({
-  code: { type: String, required: true, unique: true },
+  code: { type: String, unique: true },
   name: { type: String, required: true },
   address: { type: String },
   state: { type: String },
