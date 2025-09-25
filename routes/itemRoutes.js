20e1422bdbd0753165b6a6887b41c0abf191d200
Akash Dhiman <akashdhiman96252@gmail.com>
2025-09-25 10:03:15 +0530

    Added

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { createItem, getItems, deleteItem } = require('../controllers/itemController');
+const { createItem, getItems, deleteItem, getItemOverview } = require('../controllers/itemController');
 const router = express.Router();
 
 router.post('/', createItem);
 router.get('/', getItems);
 router.delete('/:id', deleteItem);
-router.get("/:id/overview", itemController.getItemOverview);
+router.get("/:id/overview", getItemOverview);
 
 module.exports = router;
