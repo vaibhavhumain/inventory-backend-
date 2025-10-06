ea9a843b5efe26d8949d24f99f4d9ce501090263
Akash Dhiman <akashdhiman96252@gmail.com>
2025-10-06 13:22:19 +0530

    ad

--- controllers/issueBillController.js
+++ controllers/issueBillController.js
@@ -1,4 +1,4 @@
-const Item = require("../models/Item");
+const Item = require("../models/item");
 const Bus = require("../models/Bus");
 const IssueBill = require("../models/issueBill");
 const InventoryTransaction = require("../models/InventoryTransaction");
